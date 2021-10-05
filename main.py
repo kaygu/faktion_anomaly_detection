8a6190a1d7b1a263bf6b29b5e08a39666b1eb60f
Camille DE NEEF <camille.deneef@gmail.com>
2021-10-05 23:44:12 +0200

    add data images, and reshape matrixes

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_JPGs(dir: str) -> list:
 def get_matrices(files: list) -> list:
     matrices = []
     for file in files[1:]:
-        matrices.append(cv2.imread(file))
+      matrices.append(cv2.imread(file, cv2.IMREAD_GRAYSCALE))
     return matrices
 
 if __name__ == "__main__":
@@ -28,8 +28,33 @@ if __name__ == "__main__":
     # Get dices
     anomalous_dice_files = get_JPGs('data/anomalous_dice')
     normal_dices_files = get_JPGs('data/normal_dice')
-    anomalous_matrices = get_matrices(anomalous_dice_files)
-    normal_matrices = get_matrices(normal_dices_files)
-    print(len(anomalous_matrices))
-    print(len(normal_matrices))
-    # np.save("file.npy", matrices)
+    anomalous_matrices = np.asarray(get_matrices(anomalous_dice_files))
+    normal_matrices = np.asarray(get_matrices(normal_dices_files))
+    print(anomalous_matrices[0].shape)
+    print(anomalous_matrices.shape)
+
+    plt.bar([0,1], [anomalous_matrices.shape[0], normal_matrices.shape[0]])
+    plt.title('dataset sizes')
+    plt.xticks([0,1], ['anomalous', 'normal'])
+    plt.ylabel('number of samples');
+    plt.show()
+
+
+    def plot_raster(image):
+        plt.imshow(image, cmap="gray")
+        plt.axis('off')
+        plt.show()
+
+    dog_sample = anomalous_matrices[0].reshape(28,28)
+    plot_raster(dog_sample)
+    # preprocessed_a_d= anomalous_matrices.reshape(-1,28,28)
+    # preprocessed_n_d = normal_matrices.reshape(-1,28,28)
+
+    # # Normalizing
+    # preprocessed_a_d = preprocessed_a_d/255
+    # preprocessed_n_d = preprocessed_n_d/255
+
+
+
+    # np.save("data/normal_dices.npy", normal_matrices)
+    # np.save("data/anomal_dices.npy", anomalous_matrices)
