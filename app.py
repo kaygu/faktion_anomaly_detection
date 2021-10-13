f5278caca47ad3ebdbf9a395ffcdb10ddabddb0b
jessbuenos@gmail.com <Brussel.20>
2021-10-13 11:36:03 +0200

    project almost done

--- app.py
+++ app.py
@@ -39,9 +39,6 @@ uploaded_file = st.sidebar.file_uploader('Please Upload the image of the dice', 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.title('ANOMALY DETECTION')
-st.write('This is program is going to first classify the images into twelve categories in'
-         ' base of the normal dices, After it creates a threshold mask with the image and'
-         ' compare it with the average of the whole dataset of normal images')
 
 
 def plot_from_class(prediction):
@@ -50,19 +47,20 @@ def plot_from_class(prediction):
     plt.grid(False)
     plt.xticks([])
     plt.yticks([])
-    ax.imshow(train_img_ex[train_img_index], cmap='gray', vmin=0, vmax=255)
+    ax.imshow(np.resize(train_img_ex[train_img_index], (128, 128, 1)), cmap='gray', vmin=0, vmax=255)
     class_names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     plt.xlabel("Classified in class : {}".format(class_names[predicted_label]))
     return fig
 
 
-def assert_mask(img_arr, prediction, img_size=128):
+def apply_mask(img_arr, prediction, img_size=128):
     resized_arr = cv2.resize(img_arr, (img_size, img_size))  # Reshaping images to preferred size
-    _, thresh = cv2.threshold(resized_arr, 80, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(resized_arr, 50, 255, cv2.THRESH_BINARY_INV)
     min = df[['min', 'max']].iloc[np.argmax(prediction)][0]
     max = df[['min', 'max']].iloc[np.argmax(prediction)][1]
+    meanpix = 'Mean pixel: {:.2f}'.format(np.mean(thresh))
 
-    return min < np.mean(thresh) < max
+    return min < np.mean(thresh) < max, meanpix, thresh
 
 
 if uploaded_file is not None:
@@ -73,8 +71,9 @@ if uploaded_file is not None:
     img = np.array(img)
 
     prediction = model.predict(img[None, :, :])
-    train_img_index = np.where(train_classes[0] == np.argmax(prediction))[0][0]
-    col1, col2 = st.beta_columns(2)
+    train_img_index = np.where(train_classes == np.argmax(prediction))[0]
+    col1, col2, col3= st.beta_columns(3)
+    assert_mask, meanpix, thresh = apply_mask(img[None, :, :], prediction)
 
     with col1:
         st.write('Uploaded dice:')
@@ -85,12 +84,15 @@ if uploaded_file is not None:
         plt.yticks([])
         ax.imshow(img, cmap='gray', vmin=0, vmax=255)
         st.pyplot(fig)
+        st.markdown(meanpix)
+
 
     with col2:
-        st.write('Random example of the same class')
+        st.write('Predicted class')
         st.pyplot(plot_from_class(prediction))
+        st.markdown('Class mean pixel: {:.2f}'.format(df[['mean']].iloc[np.argmax(prediction)][0]))
 
-    if assert_mask(img[None, :, :], prediction):
+    if assert_mask:
         st.info('NORMAL DICE')
     else:
         st.warning('ANOMALOUS DICE')
